d957c547a4d88d3f0a62cccd4cb78ba470050509
tkThek <gabriel.takeuchinav@post.runi.ac.il>
2024-11-19 17:55:17 +0200

    final version.

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,21 @@
 // Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	public static void main(String [] args) {
+
+		// Asks the user for the number of terms to the approximation.
+		int noOfTimes = Integer.parseInt(args[0]);
+
+		// Runs the approximation with noOfTimes terms.
+		double sum = 0.0;
+		for (int i = 0; i < noOfTimes; i++) {
+			double term = 1.0 / ((2*i) + 1); // 1/(2n+1)
+			if (i % 2 == 0) { // Alternative to the rather slow Math.pow().
+				sum += term;
+			} else {
+				sum -= term;
+			}
+		}
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + sum*4); // As the sum converges to pi/4.
 	}
 }
--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,35 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String string = args[0].toUpperCase();
+        int noOfTimes = Integer.parseInt(args[1]);
+
+        // Stores
+        String specialCases = "AEFHILMNORSX";
+
+        // Analyses the inputted string character by character.
+        String aAn = "";
+        for (int i = 0; i < string.length(); i++) {
+
+        // Checks whether the character at index i matches any characters from specialCases,
+        // then prints "Give me" + aAn accordingly.
+        // "break;" is crucial here as, otherwise, unless string.charAt(i) == "X", aAn = " a "
+        // (it goes over every letter from specialCases but does not stop until the last).
+            for (int j = 0; j < specialCases.length(); j++) {
+                 if (string.charAt(i) == specialCases.charAt(j)) {
+                    aAn = " an ";
+                    break;
+                 } else {
+                    aAn = " a ";
+                 }
+            }
+            System.out.println("Give me" + aAn + string.charAt(i) + ": " + string.charAt(i) + "!");
+        }
+
+        // Prints the final part of the output.
+        System.out.println("What does that spell?");
+        for (int i = 0; i < noOfTimes; i++) {
+                System.out.println(string + "!!!");
+        }
         }
 }
